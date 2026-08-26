32184861715d2312b90380cbf1d743325519e260
Stuart Williams <155403980+stunwill@users.noreply.github.com>
2026-08-26 20:39:29 +1000

    fix(frontend): use vitest jest-dom matchers

--- devhub/frontend/src/main.test.tsx
+++ devhub/frontend/src/main.test.tsx
@@ -1,4 +1,4 @@
-import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/vitest';
 import React from 'react';
 import {render, screen} from '@testing-library/react';
 import {describe, it, expect, vi} from 'vitest';
