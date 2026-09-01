e7382b1536b09280ed2abc2c629ccffd0d621549
Stuart Williams <155403980+stunwill@users.noreply.github.com>
2026-09-02 00:14:47 +1000

    Run DevHub through Release Execution server entrypoint

--- devhub/run.sh
+++ devhub/run.sh
@@ -32,4 +32,4 @@ fi
 
 cd /app
 alembic upgrade head
-exec uvicorn backend.app.main:app --host 0.0.0.0 --port 8099 --proxy-headers
+exec uvicorn backend.app.server:app --host 0.0.0.0 --port 8099 --proxy-headers
